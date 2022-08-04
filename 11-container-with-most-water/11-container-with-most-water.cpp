e804a00b606b253306d9e71bc209fa8e7cba0159
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-04 15:59:32 +0530

    Time: 158 ms (39.10%), Space: 59.1 MB (38.05%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,8 +5,10 @@ public:
         int i=0;
         int j=height.size()-1;
         while(i<j){
-            // int area=min(height[i],height[j])*(j-i);
-            maxi=max(min(height[i],height[j])*(j-i),maxi);
+            int width=j-i;
+            int high=min(height[i],height[j]);
+            int area=high*width;
+            maxi=max(area,maxi);
             if(height[i] > height[j])
                 j--;
             else
