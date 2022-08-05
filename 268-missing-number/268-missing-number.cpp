7b90adfe8ba17af93531255294fcb8eef8388237
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-05 09:48:37 +0530

    Time: 28 ms (63.69%), Space: 17.9 MB (64.19%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i){
+                ans=i;
+                break;
+            }
+        }return ans;
+    }
+};
