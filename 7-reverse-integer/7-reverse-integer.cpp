563c325f007c9da7da9faa036dcdf0390e528798
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-05 10:10:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.43%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
+        int last;
         int temp;
         long long int reverse;
         reverse=0;
         temp=x;
         while(abs(x)>0){
-            reverse=reverse*10+x%10;
+            last=x%10;
             x/=10;
+            reverse=reverse*10+last;
         }
         if(reverse<INT_MIN || reverse>INT_MAX){
             return 0;
