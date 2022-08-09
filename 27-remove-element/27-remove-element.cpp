f32d44d65a1e17750d67bc0840e1650b38865dba
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-09 21:18:57 +0530

    Time: 5 ms (31.79%), Space: 8.8 MB (35.48%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -4,9 +4,7 @@ public:
         int start=0;
         int count=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==val){
-                continue;
-            }else if(nums[i]!=val){
+            if(nums[i]!=val){
                 nums[start]=nums[i];
                 start++;
                 count++;
