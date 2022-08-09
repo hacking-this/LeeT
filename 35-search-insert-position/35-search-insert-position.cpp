039cfe9e80dfd407fa6fae9b0d3e4ce63fd90684
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-09 21:47:26 +0530

    Time: 14 ms (9.29%), Space: 9.7 MB (22.59%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -9,10 +9,12 @@ public:
             int mid=low+((high-low)/2);
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<target){
-                low = mid+1;
-            }else
+            }else if(nums[mid]>target){
+                
                 high=mid-1;
+            }else
+                low = mid+1;
+                //high=mid-1;
         }return low;
     }
 };
