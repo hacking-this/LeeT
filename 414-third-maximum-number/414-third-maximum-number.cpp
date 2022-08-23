7ac7540f74fafcf3a50357b3c987361aea95dc9b
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-08-23 22:15:21 +0530

    Time: 20 ms (18.63%), Space: 10.8 MB (13.91%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -5,7 +5,6 @@ public:
         vector<int> arr(s.begin(), s.end());
 
         if(arr.size()>=3) return arr[arr.size()-3];
-        else if(arr.size()==2) return arr[1];
-        else return arr[0];
+        return arr[arr.size()-1];
     }
 };
