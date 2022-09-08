e1c077f8d4c1e29c0a85ac3a43d7be23d60a6c3a
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-09-08 22:57:04 +0530

    Time: 21 ms (63.04%), Space: 10.5 MB (57.39%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int len = nums.size();
-        int a,b;
-        for(int i=0;i<len-1;i++){
-            for(int j=i+1;j<len;j++){
-                if(nums[i]+nums[j]==target){
-                    a=i;
-                    b=j;
-                    break;
-                }
+        vector<int> v1,v2;
+        v1 = nums;
+        sort(v1.begin(),v1.end());
+        int l=0;
+        int r=len-1;
+        while(l<r){
+            if (v1[l]+v1[r]==target){
+                break;
             }
-        }return {a,b};
+            else if (v1[l]+v1[r]>target) r--;
+            else l++;  
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==v1[l]) v2.push_back(i);
+            else if(nums[i]==v1[r]) v2.push_back(i);
+        }
+        
+        return v2;
+        
+        
 }
 };
