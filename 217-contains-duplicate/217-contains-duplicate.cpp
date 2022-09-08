ef69fab438f84981a3db55c8a9d51f724a4bc6c7
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-09-08 13:37:04 +0530

    Time: 171 ms (34.41%), Space: 46.4 MB (97.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        bool flag=false;
+        bool flag = false;
         for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
+            if(nums[i]==nums[i+1])
                 flag=true;
-                break;
-            }
-        }return flag;
+        }
+        return flag;
     }
 };
