8035321e963ad2483cc155cd801d786b8dd669c6
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-09-08 23:16:21 +0530

    Time: 34 ms (10.39%), Space: 15.4 MB (99.85%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n,indx1,indx2,a,b,sum;
+        int n,indx1,indx2;
         n=numbers.size();
         indx1=0;
         indx2=n-1;
-        // int result[]=new int[2];
         while(indx1<indx2){
-            sum=numbers[indx1]+numbers[indx2];
+            int sum=numbers[indx1]+numbers[indx2];
             if(sum==target){
-                a=indx1+1;
-                b=indx2+1;
                 break;
             }else if(sum<target)
                 indx1++;
             else
                 indx2--;
                 
-        }return {a,b};
+        }return {indx1+1,indx2+1};
     }
 };
