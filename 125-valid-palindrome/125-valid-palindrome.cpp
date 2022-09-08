9931ce0d07556d4640dc58ed67b3ddb0c77f664c
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-09-08 23:12:24 +0530

    Time: 25 ms (5.56%), Space: 7.6 MB (40.34%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -3,12 +3,12 @@ public:
     bool isPalindrome(string s) {
         string a;
         for(int i=0;i<s.size();i++){
-            if((s[i]>=48 && s[i]<=57)||(s[i]>=65 && s[i]<=90)||(s[i]>=97 && s[i]<=122)){
-                a+=tolower(s[i]);
-            }
-        }int i=0, j=a.size()-1;
-        while(i<j){
-            if(a[i++]!=a[j--]) return false;
+            if((s[i]>=48 && s[i]<=57)||(s[i]>=65 && s[i]<=90)||(s[i]>=97 && s[i]<=122)) a+=tolower(s[i]);
+        }
+        int l = 0;
+        int r = a.size()-1;
+        while(l<r){
+            if(a[l++]!=a[r--]) return false;
         }return true;
     }
 };
