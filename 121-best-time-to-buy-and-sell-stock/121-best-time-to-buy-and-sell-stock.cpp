426da75eafc1b76029a9b3adddb8992c76f7b829
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-09-10 22:49:32 +0530

    Time: 347 ms (5.02%), Space: 93.3 MB (89.29%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,12 +3,12 @@ public:
     int maxProfit(vector<int>& prices) {
         int mini,profit,position;
         mini=INT_MAX;
-        position=0;
-        profit=0;
+        // position=0;
+        profit=INT_MIN;
         for(int i=0;i<prices.size();i++){
             mini=min(prices[i],mini);
-            position = prices[i]-mini;
-            profit=max(position,profit);
+            // position = prices[i]-mini;
+            profit=max(prices[i]-mini,profit);
         }return profit;
     }
 };
