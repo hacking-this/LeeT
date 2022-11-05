e384d8a7dbd83d95fe69d21f5f1941b1c88fdd7e
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-05 12:32:35 +0530

    Time: 19 ms (82.88%), Space: 11.1 MB (22.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         map<int,int> mpp;
         vector<int> ans;
         
-        for(int i=0;i<nums.size();++i){
+        for(int i=0;i<nums.size();i++){
             if(mpp.find(target-nums[i])!=mpp.end()){
                 ans.push_back(mpp[target-nums[i]]);
                 ans.push_back(i); 
