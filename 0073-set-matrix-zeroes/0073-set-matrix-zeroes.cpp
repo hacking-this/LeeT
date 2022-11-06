1011b2115bdc3bec9be596a8c823595c55b01d1c
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-06 11:01:45 +0530

    Time: 19 ms (84.15%), Space: 13.4 MB (12.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         
         int n = matrix.size();
         int m = matrix[0].size();
-        set<int> row,col;
+        unordered_set<int> row,col;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
