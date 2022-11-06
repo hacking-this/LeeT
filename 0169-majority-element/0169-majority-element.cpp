3dd3f46c3ed3a62a5af0fb7572cbb97d82e0fa4e
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-06 11:02:58 +0530

    Time: 22 ms (89.47%), Space: 19.6 MB (14.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int> mpp;
-        int ans,count=0;
+        int ans;
         
         for(int i=0;i<nums.size();i++) mpp[nums[i]]++;
+        
+        int count=0;
         
         for(auto x:mpp){
             if(count<x.second){
