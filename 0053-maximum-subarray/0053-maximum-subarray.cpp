937f317d032752bb4cef45c0c4ca9e55541d8949
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-06 08:31:08 +0530

    Time: 253 ms (51.67%), Space: 67.8 MB (54.30%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,8 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi=max(sum,maxi);
+            if(sum>maxi)
+                maxi=sum;
             if(sum<0) sum=0;
         }
         return maxi;
