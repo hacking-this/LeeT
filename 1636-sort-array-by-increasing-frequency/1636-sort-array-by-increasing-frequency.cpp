f797bcae7a6e47544fa76ba811b7f796fe50fd6c
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-10 19:16:34 +0530

    Time: 17 ms (46.50%), Space: 11.2 MB (86.52%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++) mpp[nums[i]]++;
+        
+        // Lambda function
+        
+        sort(nums.begin(),nums.end(), [&](int a, int b){
+          return mpp[a]!=mpp[b] ? mpp[a]<mpp[b]: a>b; 
+        });
+        
+        return nums;
+    }
+};
