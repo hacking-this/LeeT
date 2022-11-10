234ad43a987a70fa9cd49ffb2b32f2df6c69e0cd
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-10 10:08:26 +0530

    Time: 173 ms (82.71%), Space: 33.7 MB (98.12%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(!size(nums)) return 0;
+        sort(nums.begin(),nums.end());
+        int count=1;
+        int maxi=0;
+        int n=nums.size();
+            for(int i=1;i<n;i++){
+                if(nums[i-1] == nums[i]) continue;
+                else if(nums[i]-nums[i-1]==1) count++;
+                else{
+                    maxi=max(maxi,count);
+                    count=1;
+            }
+        }
+        return max(maxi,count);
+    }
+};
