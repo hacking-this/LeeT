90c9979a4110e56808ec61c199eb1567c59d36e8
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-11 18:23:06 +0530

    Time: 23 ms (23.88%), Space: 13.7 MB (64.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
-        vector<int> ans(2,-1);
-        int low = 0;
-        int high = nums.size()-1;
-        
+    
+    int leftOccur(vector<int>& nums,int target, int low, int high){
+        int ans = -1;
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid]==target){
-                ans[0]=mid;
+                ans=mid;
                 //continue searching in left part
                 high=mid-1;
             }else if(nums[mid]>target) high=mid-1;
             else low=mid+1;
         }
-        
-        low = 0;
-        high = nums.size()-1;
-        
+        return ans;
+    }
+    
+    int rightOccur(vector<int>& nums, int target, int low, int high){
+        int ans = -1;
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid]==target){
-                ans[1]=mid;
+                ans=mid;
                 //continue searching in right part
                 low=mid+1;
             }else if(nums[mid]>target) high=mid-1;
             else low=mid+1;
         }
+        return ans;
+    }
+    
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        vector<int> ans(2,-1);
+        int low = 0;
+        int high = nums.size()-1;
+        int x = target;
+        ans[0]=leftOccur(nums,x,low,high);
+        ans[1]=rightOccur(nums,x,low,high);
         return ans;
         
     }
