6e368c58ac1bed39fd5b4cb809f944f0f5e35724
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-11 10:07:20 +0530

    Time: 34 ms (98.05%), Space: 27.6 MB (12.29%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size()-1;
         int low = 0, high = n;
         while( low <= high){
-            int mid = low + (high-low)/2;
+            int mid = (high+low)/2;
             if (nums[mid] == target) return mid;
             else if (nums[mid] > target) high = mid -1;
             else low = mid + 1;
