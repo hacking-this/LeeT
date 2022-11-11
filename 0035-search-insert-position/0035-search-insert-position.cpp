456fd9c4429ee39a23db881403b403dca78fa731
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-11 17:04:05 +0530

    Time: 2 ms (96.78%), Space: 9.7 MB (72.18%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low,high;
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                
+                high=mid-1;
+            }else
+                low = mid+1;
+                //high=mid-1;
+        }return low;
+    }
+};
