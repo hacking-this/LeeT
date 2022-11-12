eb9c59c0ef43d483fdf24c7ca72b548be64c431a
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-12 11:41:19 +0530

    Time: 123 ms (5.13%), Space: 27.3 MB (5.61%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
-                left = mid + 1;
-            else
-                right = mid;
-        }
         
-        return nums[left];
+        unordered_map<int,int> mpp;
+        
+        for(int n:nums) mpp[n]++;
+        int ans;
+        for(auto& x:mpp){
+            if(x.second==1){
+                ans=x.first;
+            }
+        }return ans;
+        
     }
 };
