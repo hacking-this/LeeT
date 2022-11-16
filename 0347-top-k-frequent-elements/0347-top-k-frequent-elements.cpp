15853d843d086bcca086171869c6d82bf14b824b
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-16 11:27:28 +0530

    Time: 42 ms (24.33%), Space: 13.7 MB (73.79%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    static bool cmp(pair<int,int>& c, pair<int,int>& d){
+        return c.second>d.second;
+    }
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> mpp;
+        for(int x:nums) mpp[x]++;
+        vector<pair<int,int>> a;
+        vector<int> ans;
+        
+        for(auto& it:mpp) a.push_back(it);
+        
+        sort(a.begin(),a.end(),cmp);
+        int i=0;
+        for(auto& it:a){ 
+            if(i<k){
+                ans.push_back(it.first);
+                i++;
+            }
+        }
+        
+        return ans;
+    }
+};
