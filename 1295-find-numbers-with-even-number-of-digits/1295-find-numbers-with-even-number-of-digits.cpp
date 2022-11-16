312b30d8e8ac6f5981c679c9843adc0a9739c55a
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-16 11:47:41 +0530

    Time: 15 ms (43.19%), Space: 9.8 MB (53.04%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-
-    int findNumbers(vector<int>& nums) {
+    int number(int n){
         int count = 0;
+        while(n>0){
+            n/=10;
+            count++;
+        }
+        return count;
+    }
+    int findNumbers(vector<int>& nums) {
+        int result = 0;
         for(int i =0; i<nums.size();i++){
-            if(nums[i]>=10 && nums[i]<=99 || nums[i]>=1000 && nums[i]<=9999 || nums[i]==100000){
-                count++;
+            if(number(nums[i])%2==0){
+                result++;
             }
-        }return count;
+        }return result;
     }
 };
