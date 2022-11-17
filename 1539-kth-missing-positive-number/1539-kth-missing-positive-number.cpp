1b38ada34ed0a4978482c0642a56d3892403de22
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-17 09:24:07 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (66.73%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        for (auto a : arr) if (a <= k) k++;
-        return k;
+    int findKthPositive(vector<int>& A, int k) {
+        int l = 0, r = A.size(), m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (A[m] - 1 - m < k)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l + k;
     }
 };
