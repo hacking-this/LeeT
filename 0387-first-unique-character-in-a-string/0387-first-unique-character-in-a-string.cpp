0b331952ee5ba7a8ca4bdaf9d61a99f4fda2dd15
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-20 12:14:47 +0530

    Time: 134 ms (12.88%), Space: 10.8 MB (16.05%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if(s.length()==1) return 0;
+        unordered_map<char,int> mpp;
+        for(char x:s) mpp[x]++;
+        for(int i=0;i<s.length();i++){
+            if(mpp.count(s[i])){
+                if(mpp[s[i]]==1) return i;
+            }
+        }
+        return -1;
+        
+    }
+};
