ca3f17367a546c25245013d903f3710513dacb2e
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-23 12:03:42 +0530

    Time: 24 ms (91.37%), Space: 18.1 MB (72.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<string,int>hash;
-        for(int row=0;row<9;row++){
-            for(int col=0;col<9;col++){
-                if(board[row][col] != '.')
-                {
-    
-                    string r= "row"+to_string(row)+to_string(board[row][col]);
-                    string c= "col"+to_string(col)+to_string(board[row][col]);
-                    int b = (row/3) * 3 + (col/3);
-                    string box= "box"+ to_string(b) + to_string(board[row][col]);
-
-                    if(hash[r] != 1 && hash[c] != 1 && hash[box] != 1){
-                        hash[r]=1;
-                        hash[c]=1;
-                        hash[box]=1;
-                    }else{
-                        return false;
-                    }
-
-                    }
+        int row[10]={0}, col[10]={0}, dia[10]={0};
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                int val=board[i][j]-'0';
+                int pos=1<<val;
+                int di = (i/3)*3 + (j/3); 
                 
+                if(row[i] & pos) return false;
+                if(col[j] & pos) return false;
+                if(dia[di] & pos) return false;
+                
+                row[i] |=pos;
+                col[j] |=pos;
+                dia[di] |=pos;
             }
         }
         
