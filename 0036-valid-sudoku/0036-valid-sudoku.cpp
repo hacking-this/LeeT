3edea9b9d917a0eb710554d210d5672940b6b58d
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-23 23:28:04 +0530

    Time: 60 ms (23.72%), Space: 20.5 MB (21.72%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int row[10]={0}, col[10]={0}, dia[10]={0};
+        vector<set<int>> row(9),col(9),box(9);
         
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(board[i][j]=='.'){
-                    continue;
+        for(int i=0;i< 9;i++)
+        {
+            for(int j = 0;j < 9;j++)
+            {
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0';
+                    
+                    if(row[i].count(num) || col[j].count(num) || box[(i/3)*3 + (j/3)].count(num))
+                    {
+                        return false;
+                    }
+                    
+                    row[i].insert(num);
+                    col[j].insert(num);
+                    box[(i/3)*3 + (j/3)].insert(num);
+            
                 }
-                int val=board[i][j]-'0';
-                int pos=1<<val;
-                int di = (i/3)*3 + (j/3); 
-                
-                if(row[i] & pos) return false;
-                if(col[j] & pos) return false;
-                if(dia[di] & pos) return false;
-                
-                row[i] |=pos;
-                col[j] |=pos;
-                dia[di] |=pos;
             }
         }
         
         return true;
-    }
+        }
 };
