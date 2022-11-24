6eca86bb3eb50430f8861e5e1afcb78238cdf1bb
hacking-this <108381779+hacking-this@users.noreply.github.com>
2022-11-24 17:54:26 +0530

    Time: 13 ms (93.01%), Space: 7.7 MB (39.84%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,9 +9,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        auto next = node->next;
-        *node = *next;
-        delete next;
-        
+        node->val = node->next->val;
+        node->next = node->next->next;
     }
 };
